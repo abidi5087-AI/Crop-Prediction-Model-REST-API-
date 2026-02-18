9d019e15a265ff0d3d287be99cf3e19717424416
SYED TAQI BAQAR ABIDI <shamikhabidi4@gmail.com>
2026-02-18 10:49:54 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 import numpy as np
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 import pickle
+import os
 
-app=Flask(__name__)
-model=pickle.load(open("model.pkl","rb"))
+# Vercel ko 'app' naam hi chahiye hota hai
+app = Flask(__name__)
+
+# Model file ka path sahi se set karne ke liye
+model_path = os.path.join(os.path.dirname(__file__), 'model.pkl')
+model = pickle.load(open(model_path, 'rb'))
+
 @app.route("/")
 def Home():
     return render_template("index.html")
-@app.route("/predict",
-methods=["POST"])
-def predict():
-    float_features=[float(x) for x in request.form.values()]
-    features=[np.array(float_features)]
-    prediction=model.predict(features)
-    return render_template("index.html",prediction_text="The Predicted Crop is {}".format(prediction))
-if __name__=="__main__":
-    flask_app.run(debug=True)
 
+@app.route("/predict", methods=["POST"])
+def predict():
+    float_features = [float(x) for x in request.form.values()]
+    features = [np.array(float_features)]
+    prediction = model.predict(features)
+    return render_template("index.html", prediction_text="The Predicted Crop is {}".format(prediction))
+
+if __name__ == "__main__":
+    app.run(debug=True)
