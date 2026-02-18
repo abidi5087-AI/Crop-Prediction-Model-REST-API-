f3f41443fc5c8c959cc61fd98aacb50e0a55112c
SYED TAQI BAQAR ABIDI <shamikhabidi4@gmail.com>
2026-02-18 11:19:56 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask import Flask, request, render_template
 import pickle
 import os
 
-# Vercel ko 'app' naam hi chahiye hota hai
 app = Flask(__name__)
 
-# Model file ka path sahi se set karne ke liye
-model_path = os.path.join(os.path.dirname(__file__), 'model.pkl')
+# Model file ka rasta (path) dhoondne ke liye
+base_dir = os.path.dirname(__file__)
+model_path = os.path.join(base_dir, 'model.pkl')
+
+# Model load karne ka surakshit tareeka
 model = pickle.load(open(model_path, 'rb'))
 
 @app.route("/")
@@ -16,9 +18,13 @@ def Home():
 
 @app.route("/predict", methods=["POST"])
 def predict():
+    # Input data lena
     float_features = [float(x) for x in request.form.values()]
     features = [np.array(float_features)]
+    
+    # Prediction karna
     prediction = model.predict(features)
+    
     return render_template("index.html", prediction_text="The Predicted Crop is {}".format(prediction))
 
 if __name__ == "__main__":
