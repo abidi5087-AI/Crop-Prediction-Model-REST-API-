68d146e9b188e70d4bedda00b38c8aa7b0e82a2d
SYED TAQI BAQAR ABIDI <shamikhabidi4@gmail.com>
2026-02-18 12:23:38 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-
-   
-    #import numpy as np
+import numpy as np
 from flask import Flask, request, render_template
 import pickle
 import os
 
 app = Flask(__name__)
 
-# Model path globally set kar dete hain
+# Model load globally
 model_path = os.path.join(os.path.dirname(__file__), 'model.pkl')
+model = pickle.load(open(model_path, 'rb'))
 
 @app.route("/")
 def Home():
@@ -17,25 +16,13 @@ def Home():
 @app.route("/predict", methods=["POST"])
 def predict():
     try:
-        # Jugaad: Model ko function ke andar hi load karo
-        if not os.path.exists(model_path):
-            return render_template("index.html", prediction_text="Error: model.pkl file nahi mili!")
-            
-        with open(model_path, 'rb') as f:
-            local_model = pickle.load(f)
+        # Form se data lena
+        float_features = [float(x) for x in request.form.values()]
+        features = [np.array(float_features)]
         
-        # Form se data nikalna
-        input_data = [float(x) for x in request.form.values()]
-        features = [np.array(input_data)]
-        
-        # Prediction karna
-        prediction = local_model.predict(features)
+        # Prediction
+        prediction = model.predict(features)
         
         return render_template("index.html", prediction_text="The Predicted Crop is: {}".format(prediction[0]))
-    
     except Exception as e:
-        # Jo bhi galti hogi ab screen par dikhegi
         return render_template("index.html", prediction_text="Error: {}".format(str(e)))
-
-if __name__ == "__main__":
-    app.run(debug=True)
